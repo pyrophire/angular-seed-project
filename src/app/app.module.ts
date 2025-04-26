c6b5dcb86289d795dae3f63439ff8a28e5a1fe2c
Eli <elijah.fernandez@tlc.texas.gov>
2025-04-26 09:35:14 -0500

    Update dependencies and refactor toast service integration; migrate to @ngxpert/hot-toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MatIconRegistry } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HotToastModule } from '@ngneat/hot-toast';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -11,13 +11,13 @@ import { httpInterceptorProviders } from './shared/interceptors';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, MaterialModule, HotToastModule.forRoot()],
-  providers: [httpInterceptorProviders],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, MaterialModule],
+    providers: [httpInterceptorProviders, provideHotToastConfig()],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(iconRegistry: MatIconRegistry) {
-    iconRegistry.setDefaultFontSetClass('material-symbols-outlined');
-  }
+    constructor(iconRegistry: MatIconRegistry) {
+        iconRegistry.setDefaultFontSetClass('material-symbols-outlined');
+    }
 }
